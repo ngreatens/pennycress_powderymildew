a385d1c333911101a6d9eb2d6000ae92ffd802ee
Nick Greatens <nicholasgreatens@gmail.com>
2024-08-21 13:48:58 -0400

    Add files via upload

--- ggtree.R
+++ ggtree.R
@@ -7,7 +7,6 @@ library(treeio)
 #read in tree
 tree <- read.iqtree("tree.nwk")
 
-
 #read in csv
 df <-read.csv("erysiphe_pennycress_datatable.csv")
 
@@ -23,7 +22,8 @@ d <- d[!d$isTip,]
 d$UFboot <- as.numeric(d$UFboot)
 d80 <- d[d$UFboot>=80,]
 
-cladeoffset=0.0425
+cladeoffset=0.045
+fontsize=3
 
 #tree
 p<- ggtree(tree)  %<+% df +
@@ -34,15 +34,15 @@ p<- ggtree(tree)  %<+% df +
     label.size = 0,
     label.padding = unit(0, "lines"),
     parse = TRUE,
-    size = 2.5,
+    size = fontsize,
     show.legend = FALSE,
     align = FALSE
   ) +
-  geom_cladelab(node=26, label = " Erysiphe alliariicola", offset = cladeoffset,  align = TRUE, fontface=3, fontsize = 2.5) +
-  geom_cladelab(node=31, label = " Erysiphe crucifearum", offset = cladeoffset, fontsize = 2.5, align = TRUE, fontface=3) +
-  geom_cladelab(node=22, label = " Erysiphe radulescui", offset = cladeoffset, fontsize = 2.5, align = TRUE, fontface=3) +
-  geom_striplab('s1','s2', label = " Erysiphe intermedia", offset = cladeoffset, fontsize = 2.5, align = TRUE, fontface=3) +
+  geom_cladelab(node=26, label = " Erysiphe alliariicola", offset = cladeoffset,  align = TRUE, fontface=3, fontsize = fontsize) +
+  geom_cladelab(node=31, label = " Erysiphe crucifearum", offset = cladeoffset, fontsize = fontsize, align = TRUE, fontface=3) +
+  geom_cladelab(node=22, label = " Erysiphe radulescui", offset = cladeoffset, fontsize = fontsize, align = TRUE, fontface=3) +
+  geom_striplab('s1','s2', label = " Erysiphe intermedia", offset = cladeoffset, fontsize = fontsize, align = TRUE, fontface=3) +
   xlim(0,.1) + 
-  geom_text2(data = d80, aes(label = UFboot), size = 2, hjust = 1.2, vjust = -.5) 
+  geom_text2(data = d80, aes(label = UFboot), size = 2.5, hjust = 1.1, vjust = -.5) 
 #save to tif image file.
 p
